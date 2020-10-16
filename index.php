efebc0f2071bfdd19f78aa97c9e3e877effe8e7a
mcbrito1 <46609862+mcbrito1@users.noreply.github.com>
2020-10-15 23:07:19 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Desenvolvimento para a Web II - 3A - 2/2020</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+      echo 'Hello World';
+    ?>
+</body>
+</html>
